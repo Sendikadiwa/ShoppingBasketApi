2c5a827dc4a3a1750a17a3518b0416890d6c92e6
Ssendikadiwa <sendi.stev@gmail.com>
2019-06-05 04:55:26 +0300

    Add functionality to register user

---
+++ routes/auth.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
+const bcrypt = require('bcryptjs');
+
+// Load User model
+const User = require('../models/User');
+
+router.post(
+  '/register',
+  [
+    check('name', 'Name is required')
+      .trim()
+      .not()
+      .isEmpty(),
+    check('email', 'Please use a valid email')
+      .trim()
+      .isEmail(),
+    check('password', 'Password is required')
+      .trim()
+      .isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, password } = req.body;
+    try {
+      // check if user exists using email
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ message: 'Email already in use' });
+      }
+      // user doesnot exist-> create one
+      user = new User({
+        name,
+        email,
+        password
+      });
+      // Encrypt password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      // save user to database
+      await user.save();
+
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server Error!');
+    }
+  }
+);
+
+module.exports = router;

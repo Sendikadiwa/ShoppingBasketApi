0fdea96ef89465d6c056e4d86e9733de960549f4
Ssendikadiwa <sendi.stev@gmail.com>
2019-06-08 12:00:18 +0300

    Add create basket functionality

--- routes/api/basket.js
+++ routes/api/basket.js
@@ -1,12 +1,40 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator/check');
 
-router.get('/', auth, (req, res) => {
-  try {
-    return res.send('Authorized');
-  } catch (error) {
-    res.status(401).json('Unauthorized');
+// load users and basket models
+const User = require('../../models/User');
+const Basket = require('../../models/Basket');
+
+router.post(
+  '/',
+  [
+    check('name', 'Basket name is missing')
+      .trim()
+      .not()
+      .isEmpty()
+  ],
+  auth,
+  async (req, res) => {
+    // check for errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      // create a new basket object
+      const basketData = new Basket({ name: req.body.name, user: req.user.id });
+
+      // save data to db
+      await basketData.save();
+
+      res.json(basketData);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server Error');
+    }
   }
-});
+);
+
 module.exports = router;

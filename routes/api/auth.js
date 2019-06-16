cc0896026b593af243cf9a99f61fb89158577e3f
Ssendikadiwa <sendi.stev@gmail.com>
2019-06-16 16:57:56 +0300

    Add route to get authenticated user

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,6 +3,25 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { check, validationResult } = require('express-validator/check');
+const auth = require('../../middleware/auth');
+
+const User = require('../../models/User');
+
+/*
++End-point:   POST/auth
++Description: Get authenticated user
++Access:      Private
+*/
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 /*
 +End-point:   POST/auth
 +Description: Enables authenticateduser to login and get get

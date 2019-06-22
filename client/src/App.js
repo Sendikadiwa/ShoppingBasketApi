60283476f433107778cbefd9d7dbec73bae16a76
sendistephen <sendi.steve2@gmail.com>
2019-06-22 17:56:35 +0300

    Add Private route

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,41 @@
-import React, { Fragment, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import './App.css';
-import Navbar from './components/layouts/Navbar';
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
-import store from './store';
-import Alert from './components/layouts/Alert';
-import setAuthToken from './utils/setAuthToken';
-import { loadUser } from './actions/auth';
+import React, { Fragment, useEffect } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import './App.css'
+import Navbar from './components/layouts/Navbar'
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
+import store from './store'
+import Alert from './components/layouts/Alert'
+import setAuthToken from './utils/setAuthToken'
+import { loadUser } from './actions/auth'
+import PrivateRoute from './components/routing/PrivateRoute'
+import Dashboard from './components/dashboard/Dashboard'
 
 if (localStorage.token) {
-  setAuthToken(localStorage.token);
+	setAuthToken(localStorage.token)
 }
 const App = () => {
-  useEffect(() => {
-    store.dispatch(loadUser());
-  }, []);
-  return (
-    <Provider store={store}>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className='container'>
-            <Alert />
-            <Switch>
-              <Route exact path='/' component={Register} />
-              <Route exact path='/login' component={Login} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </Provider>
-  );
-};
+	useEffect(() => {
+		store.dispatch(loadUser())
+	}, [])
+	return (
+		<Provider store={store}>
+			<Router>
+				<Fragment>
+					<Navbar />
+					<div className='container'>
+						<Alert />
+						<Switch>
+							<Route exact path='/' component={Register} />
+							<Route exact path='/login' component={Login} />
+							<PrivateRoute exact path='/dashboard' component={Dashboard} />
+						</Switch>
+					</div>
+				</Fragment>
+			</Router>
+		</Provider>
+	)
+}
 
-export default App;
+export default App

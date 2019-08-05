df8cc8da64533392bc8b97c8110df761ea325de2
sendistephen <sendi.steve2@gmail.com>
2019-08-05 16:55:22 +0300

    Add tests to validate user email

--- tests/integration/users.test.js
+++ tests/integration/users.test.js
@@ -36,7 +36,7 @@ describe('/api/v1/users', () => {
 				.send({ name, email, password });
 		};
 
-		it('Should return 400 if user registers with invalid username', async () => {
+		it('Should return 400 if user registers with username less than 3 characters', async () => {
 			name = 'a';
 			email = 'kats@example.com';
 			password = '73&6ehehfkk';
@@ -45,5 +45,61 @@ describe('/api/v1/users', () => {
 
 			expect(res.status).toBe(400);
 		});
+		it('Should return 400 if user registers with username morethan 50 characters', async () => {
+			name = new Array(54).join('w');
+			email = 'kats@example.com';
+			password = '73&6ehehfkk';
+
+			const res = await exec();
+
+			expect(res.status).toBe(400);
+		});
+		it('Should return 400 if user registers with empty spaces for username', async () => {
+			name = '   ';
+			email = 'kats@example.com';
+			password = '73&6ehehfkk';
+
+			const res = await exec();
+
+			expect(res.status).toBe(400);
+		});
+		it('Should return 400 if user registers with an invalid email', async () => {
+			name = 'kats@example.com';
+			email = 'kats.example';
+			password = '73&6ehehfkk';
+
+			const res = await exec();
+
+			expect(res.status).toBe(400);
+		});
+		it('Should return 400 if user registers with white spaces for email', async () => {
+			name = 'kats@example.com';
+			email = '  ';
+			password = '73&6ehehfkk';
+
+			const res = await exec();
+
+			expect(res.status).toBe(400);
+		});
+		it('Should return 400 if user registers with an email less than 10 characters', async () => {
+			name = 'Namutebi';
+			email = 'k@g.com';
+			password = '73&6ehehfkk';
+
+			const res = await exec();
+
+			expect(res.status).toBe(400);
+		});
+		it('Should return 400 if user registers with an email that already exists', async () => {
+			name = 'Namutebi';
+			email = 'sendi.stev@gmail.com';
+			password = '73&6ehehfkk';
+
+			const res = await exec();
+
+			expect(res.status).toBe(400);
+        });
+        
+
 	});
 });

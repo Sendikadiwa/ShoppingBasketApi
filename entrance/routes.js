b4db37356ce229c1e1e758a5483289ba295a5e98
sendistephen <sendi.steve2@gmail.com>
2019-08-08 21:33:57 +0300

    Add error middleware to app

--- entrance/routes.js
+++ entrance/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const users = require('../routes/api/users');
 const auth = require('../routes/api/auth');
 const baskets = require('../routes/api/baskets');
+const error = require('../middleware/error');
 
 module.exports = function(app) {
 	app.use(express.json({ extended: false })); // parsing json requests
@@ -10,4 +11,6 @@ module.exports = function(app) {
 	app.use('/api/v1/users', users);
 	app.use('/api/v1/auth', auth);
 	app.use('/api/v1/baskets', baskets);
+
+	app.use(error);
 };

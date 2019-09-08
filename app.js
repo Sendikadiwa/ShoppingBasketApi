b8cdd7df8ff6a56aae6797f4f501fd0e18af60bd
sendistephen <sendi.steve2@gmail.com>
2019-09-08 18:11:49 +0300

    Enable cors

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const config = require("config");
+const cors = require("cors");
 const dbConnection = require("./entrance/db");
 const logger = require("./entrance/log");
 
 const app = express();
 // Db connection
 dbConnection();
-
+// enable all cors requests
+app.use(cors());
 // routes
 require("./entrance/routes")(app);
 
